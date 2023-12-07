60c6cc9a4e7cb880d05b84763b5f6aed57ba5594
krishnisha <nishadurai98@gmail.com>
2023-12-07 01:47:50 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const cors= require("cors")
+const express= require("express");
+const mongo = require("./shared/mongo")
+const app=express();
+const resetRoute=require("./Routes/resetPassword.routes");
+const userRoutes=require("./Routes/users.routes");
+(async()=>{
+
+
+try{
+
+    await mongo.connect()
+
+    app.use(cors())
+
+    app.use(express.json())
+
+
+
+const port= process.env.PORT || 3001;
+
+app.listen(port,()=>{
+        console.log("server running in port ",port)
+ })
+}
+catch(err)
+{
+    console.log("Error in connecting DB",err)
+}
+})()
